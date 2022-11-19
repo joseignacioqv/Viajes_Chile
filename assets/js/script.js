a9e2fb1ee0b9851c089a914ff048ac452644fd17
Jose Ignacio Quezada <joseignacioqv@gmail.com>
2022-11-19 14:55:42 -0300

    barra negra habilitada al hacer scroll

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,5 +4,16 @@ $(function(){ // Inicio de function//
     // tooltip "enviar correo" boton de cabecera //
     const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
     const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+
+  var nav = document.querySelector('nav');
+
+  window.addEventListener('scroll', function() {
+  if (window.pageYOffset >100) {
+    nav.classList.add('bg-dark', 'shadow');
+   } else {
+    nav.classList.remove('bg-dark', 'shadow');
+   }
+  });
+
     
     }); // cierre de function //
