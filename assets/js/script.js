c2fe31428a71db1271b5c5288b88d76b8c2e85c4
Jose Ignacio Quezada <joseignacioqv@gmail.com>
2022-11-19 18:16:16 -0300

    version final

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,9 +2,15 @@ $(function(){ // Inicio de function//
 
 
     // tooltip "enviar correo" boton de cabecera //
+
     const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
     const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+    // popover //
+    
+    const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]')
+    const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl))
 
+    // Al hacer scroll para a negro el navbar //
   var nav = document.querySelector('nav');
 
   window.addEventListener('scroll', function() {
