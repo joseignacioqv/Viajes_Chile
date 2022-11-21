384f7e7182c9392f07da7f86d0050f91e4dde7b4
Jose Ignacio Quezada <joseignacioqv@gmail.com>
2022-11-21 19:52:06 -0300

    version final

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,20 +6,19 @@ $(function(){ // Inicio de function//
     const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
     const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
     // popover //
-    
+
     const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]')
     const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl))
 
     // Al hacer scroll para a negro el navbar //
-  var nav = document.querySelector('nav');
+    var nav = document.querySelector('nav');
 
-  window.addEventListener('scroll', function() {
-  if (window.pageYOffset >100) {
+    window.addEventListener('scroll', function() {
+    if (window.pageYOffset >100) {
     nav.classList.add('bg-dark', 'shadow');
-   } else {
+    } else {
     nav.classList.remove('bg-dark', 'shadow');
-   }
-  });
+    }
+    });
 
-    
     }); // cierre de function //
